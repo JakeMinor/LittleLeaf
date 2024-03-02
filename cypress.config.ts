bb9783ea96f124f33113c7379cf2fa7cf24f0f60
Jake <jacobminor@hotmail.co.uk>
2024-03-02 16:27:07 +0000

    Add template tests to stop pipeline failing

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "cypress";
-
 export default defineConfig({
+  video: false,
+  screenshotOnRunFailure: false,
+  viewportWidth: 1920,
+  viewportHeight: 720,
+  reporter: "junit",
+  reporterOptions: {
+    testsuitesTitle: true,
+    mochaFile: "./test-results/cypress.[hash].xml"
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: "**/**.cy.ts",
   },
 });
